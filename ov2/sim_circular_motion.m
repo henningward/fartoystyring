bf111d8c29bb1e8827a53f9d446fcd977812004b
henningward <wardhenning@gmail.com>
2017-09-07 11:08:35 +0200

    matlab fix

--- ov2/sim_circular_motion.m
+++ ov2/sim_circular_motion.m
@@ -5,59 +5,125 @@ clc;
 deg2rad = pi/180;   
 rad2deg = 180/pi;
 
-
 %model parameters
-phi = 0*deg2rad;
-theta = 2*deg2rad;
-psi = 30*deg2rad;
+phi = 0 * deg2rad;
+theta = 2.0 * deg2rad;
+psi = 30.0 * deg2rad;
 U = 1.5;
+w = 360*U/(2*pi*100) * deg2rad;
+
+%current parameters
 U_c = 0.6;
 alpha_c = 10*deg2rad;
 beta_c = 45*deg2rad;
 
 
+R_nb = Rzyx(phi,theta,psi); %http://planning.cs.uiuc.edu/node102.html
+
+%init pos
+pos = [0 0 0]';
+
+u_nc = U_c*cos(alpha_c)*cos(beta_c);
+v_nc = sin(beta_c)*U_c;
+w_nc = U*sin(alpha_c)*cos(beta_c);
+
+%currents in NED frame
+V_nc_vect = [u_nc v_nc w_nc]';
+
+%currents in BODY frame
+V_bc_vect = inv(R_nb) * V_nc_vect;
+
+
 
 %simulation parameters
-N = 1000;
+N = 50000;
 h = 0.1;
 
-w = 360*U/(2*pi*100);
-table = zeros(N+1,10);
+%memory allocation
+table = zeros(N+1,8);
+angle_table = zeros(N+1, 3);
 
-
-pos = [0 0 0]';
-u_c = U_c*cos(alpha_c)*cos(beta_c);
-v_c = sin(beta_c)*U_c;
-w_c = U*sin(alpha_c)*cos(beta_c);
-U_c_vect = [u_c v_c w_c]';
-
+%% WITHOUT CURRENT
 for i = 1:N+1,
     t = (i-1)*h;
-    U_vect = [U*cos(w*t) U*sin(w*t) 0]';
-    U_r_vect = U_vect + U_c_vect;
     
-    pos = pos+h*U_r_vect;
+    %velocity of body in BODY frame, relative to NED
+    V_bb = [U*cos(w*t) U*sin(w*t) 0]';
     
-    table(i,:) = [t U_vect' pos' U_r_vect'];
-
+    %velocity of body in NED frame, relative to NED
+    V_nb = R_nb * V_bb;
+    
+    %calculation of position at current timestep in BODY frame
+    pos = pos+h*V_nb;
+    
+    %calculation of speed, equal to the norm of velocity in BODY frame
+    speed = norm(V_bb);
+    
+    
+    crab_angle = asin(V_nb(2)/speed) *rad2deg;
+    sideslip_angle = asin(V_nb(2)/speed)*rad2deg; 
+    course_angle = (psi + crab_angle)*rad2deg;
+    
+    
+    
+    table(i,:) = [t V_nb' pos' speed];
+    
+    angle_table(1,:) = [course_angle crab_angle sideslip_angle];
 end
 
-
-%{
-for i = 1:N+1,
-    t = (i-1)*h;
-    %U_vect = [U*cos(w*t) U*sin(w*t) 0]' + [U_c*cos(a_c) U_c*sin(b_c) U_c*sin(a_c)*cos(b_c]';
-
-
-
-end
-
-%}
-
-U         = table(:,2:4);
 t         = table(:,1);
-pos       = table(:,5:7);
-U_r       = table(:,8:10);
+rel_speed = table(:,2:4);
+position  = table(:,5:7);
+speed     = table(:,8);
+
 
 figure(1)
-plot(pos(:,2), pos(:,1)),xlabel('East'),ylabel('North'),title('position'),grid
+plot(position(:,2), position(:,1)),xlabel('East'),ylabel('North'),title('position'),grid
+
+
+
+
+
+
+
+%% WITH CURRENT
+
+%init pos
+pos = [0 0 0]';
+
+for i = 1:N+1,
+    t = (i-1)*h;
+    
+    %velocity of body in BODY frame, relative to NED
+    V_bb = [U*cos(w*t) U*sin(w*t) 0]' + V_bc_vect;
+    
+    %velocity of body in NED frame, relative to NED
+    V_nb = R_nb * V_bb;
+    
+    %calculation of position at current timestep in BODY frame
+    pos = pos+h*V_nb;
+    
+    %calculation of speed, equal to the norm of velocity in BODY frame
+    speed = norm(V_bb);
+    
+    
+    crab_angle = asin(V_nb(2)/speed) *rad2deg;
+    sideslip_angle = asin(V_nb(2)/speed)*rad2deg; 
+    course_angle = (psi + crab_angle)*rad2deg;
+    
+    
+    
+    table(i,:) = [t V_nb' pos' speed];
+    
+    angle_table(1,:) = [course_angle crab_angle sideslip_angle];
+end
+
+t         = table(:,1);
+rel_speed = table(:,2:4);
+position  = table(:,5:7);
+speed     = table(:,8);
+
+
+figure(2)
+plot(position(:,2), position(:,1)),xlabel('East'),ylabel('North'),title('position'),grid
+
