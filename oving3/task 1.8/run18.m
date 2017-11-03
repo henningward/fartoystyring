d7226904f6c2e384deb8899c55fa59bc2f080920
Henning Ward <henningward@Hennings-MacBook-Pro.local>
2017-11-03 08:48:18 +0100

    latest commit

--- oving3/task 1.8/run18.m
+++ oving3/task 1.8/run18.m
@@ -43,7 +43,7 @@ p0=zeros(2,1);      % Initial position (NED)
 v0=[6.63 0]';       % Initial velocity (body)
 psi0=0;             % Inital yaw angle
 r0=0;               % Inital yaw rate
-c=1;                % Current on (1)/off (0)
+c=0;                % Current on (1)/off (0)
 
 %1.2 finding optimal Nomoto-parameters 
 
@@ -60,12 +60,46 @@ Kd = (2*omega_0*T-1)/K;
 Ki = omega_0/10*Kp;
 
 
+
+%calculating d1 and d2 for speeed controller
+
+%arbitrary values for nc to obtain two equation sets, needed to calculate
+%d1 and d2
+%1st equation set
+nc = 5;
 sim MSFartoystyring18
+nc_1 = nc;
+u_1 = v(end, 1);
+%2nd equation set
+nc = 71;
+sim MSFartoystyring18
+nc_2 = nc;
+u_2 = v(end, 1);
+
+u = [u_1 u_1*abs(u_1);
+     u_2 u_2*abs(u_2)];
+n = [nc_1*abs(nc_1);
+     nc_2*abs(nc_2)];
+d = inv(u)*n;
+d_1 = d(1);
+d_2 = d(2);
+
+
+
+syms d1 d2;
+Q1 = d1*u_1+d2*abs(u_1)*u_1 == abs(nc_1)*nc_1;
+Q2 = d1*u_2+d2*abs(u_2)*u_2 == abs(nc_2)*nc_2;
+
+sol = solve([Q1 Q2], [d1 d2], 'ReturnConditions', true);
+
+d3 = sol.d1
+d4 = sol.d2
+
 
 
 figure()
 plot(t,v);
-legend({'u', 'v'}, 'Interpreter','latex')
+legend({'u' ,'v'}, 'Interpreter','latex')
 xlabel('time (s)')
 ylabel('rudder input [deg]')
 title('Rudder input $\delta_c$','Interpreter','latex','FontSize',16)
